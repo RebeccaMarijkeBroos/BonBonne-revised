37c795613e9af4e1bb47f80c0fde357a02175c30
MarijkeBroos <broos.marijke@gmail.com>
2019-04-04 10:52:47 +0200

    Updates

---
+++ precache-manifest.cde163510ca4ff6f736cdfc61581fd79.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e8c28c468b5f250f853263f06a3b1836",
+    "url": "/BonBonne-revised/static/media/restaurant.e8c28c46.jpeg"
+  },
+  {
+    "revision": "bff0b546b93e732896f1",
+    "url": "/BonBonne-revised/static/js/runtime~main.3a6032a7.js"
+  },
+  {
+    "revision": "25e9bb9eacf40244a189",
+    "url": "/BonBonne-revised/static/js/main.68d60c50.chunk.js"
+  },
+  {
+    "revision": "d90a1a4ba5aef2bc17ad",
+    "url": "/BonBonne-revised/static/js/2.8ac732ef.chunk.js"
+  },
+  {
+    "revision": "25e9bb9eacf40244a189",
+    "url": "/BonBonne-revised/static/css/main.8463f6de.chunk.css"
+  },
+  {
+    "revision": "efd081d0ad1146df03cdec197902fe1f",
+    "url": "/BonBonne-revised/index.html"
+  }
+];
